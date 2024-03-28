73cbbf96f676b144c23294805407324836effe4c
Francisco Valdes <fvaldesrojas99@gmail.com>
2024-03-28 12:49:18 -0300

    Funcionando con OTA

--- pn532 funcionando con internet y ota/PN532_SPI_modular_Funcionando_con_Internet_OTA/PN532_SPI_modular_Funcionando_con_Internet_OTA.ino
+++ pn532 funcionando con internet y ota/PN532_SPI_modular_Funcionando_con_Internet_OTA/PN532_SPI_modular_Funcionando_con_Internet_OTA.ino
@@ -3,11 +3,10 @@
 #include <SPI.h> //Librería para comunicación SPI
 #include <UNIT_PN532.h> //Librería Modificada
 #include <WiFi.h>
+#include <ESPmDNS.h>
+#include <WiFiUdp.h>
+#include <ArduinoOTA.h> // librerias de OTA (over the air esp32)
 #include <HTTPClient.h>
-#include <ArduinoJson.h>
-#include <vector> // Incluir la biblioteca para utilizar std::vector
-// Variable global para almacenar los valores JSON
-std::vector<String> valoresJSON;
 
 //Conexiones SPI del ESP32
 #define PN532_SCK  (18)
@@ -66,7 +65,40 @@ void setup(){
   delay(200);
   Serial.println("ESP32 Iniciado");
   IniciarInternet();
-  GETJSON();
+  
+  
+  ArduinoOTA
+    .onStart([]() {
+      String type;
+      if (ArduinoOTA.getCommand() == U_FLASH)
+        type = "sketch";
+      else // U_SPIFFS
+        type = "filesystem";
+
+      // NOTE: if updating SPIFFS this would be the place to unmount SPIFFS using SPIFFS.end()
+      Serial.println("Start updating " + type);
+    })
+    .onEnd([]() {
+      Serial.println("\nEnd");
+    })
+    .onProgress([](unsigned int progress, unsigned int total) {
+      Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+    })
+    .onError([](ota_error_t error) {
+      Serial.printf("Error[%u]: ", error);
+      if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
+      else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
+      else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
+      else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
+      else if (error == OTA_END_ERROR) Serial.println("End Failed");
+    });
+
+  ArduinoOTA.begin();
+
+  Serial.println("Ready");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+  
 
   iniciarLectores();
 
@@ -74,6 +106,7 @@ void setup(){
 
 
 void loop(){
+  ArduinoOTA.handle();
   leer();
 
 }
@@ -261,6 +294,7 @@ void enviarNumero(int num) {
 }
 
 void IniciarInternet() {
+  WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED) {
     delay(1000);
@@ -269,44 +303,6 @@ void IniciarInternet() {
   Serial.println("Conectado a la red WiFi");
   Serial.print("Dirección IP: ");
   Serial.println(WiFi.localIP());
-}
-
-void GETJSON() {
-  HTTPClient http;
-
-  // Dirección URL a la que deseas enviar la solicitud GET
-  String url = "http://192.168.31.120:8500/arduino/";
-
-  Serial.print("Enviando solicitud GET a: ");
-  Serial.println(url);
-
-  http.begin(url);  // Especificar URL
-  int httpCode = http.GET();  // Realizar la solicitud GET
-
-  if (httpCode > 0) { // Verificar si la solicitud fue exitosa
-    Serial.printf("[HTTP] Código de estado: %d\n", httpCode);
-    String payload = http.getString();   // Obtener respuesta del servidor
-    Serial.println("Respuesta del servidor:");
-    Serial.println(payload);             // Mostrar respuesta en el puerto serie
-
-    // Analizar JSON
-    StaticJsonDocument<512> jsonDocument;
-    DeserializationError error = deserializeJson(jsonDocument, payload);
-    if (error) {
-      Serial.print("Error al analizar JSON: ");
-      Serial.println(error.c_str());
-    } else {
-      // Extraer los valores del JSON y almacenarlos en el vector global
-      valoresJSON.clear(); // Limpiar el vector antes de agregar nuevos valores
-      for (JsonPair kvp : jsonDocument.as<JsonObject>()) {
-        valoresJSON.push_back(kvp.value().as<String>());
-      }
-    }
-  } else {
-    Serial.printf("[HTTP] Error: %s\n", http.errorToString(httpCode).c_str());
-  }
-
-  http.end();  // Finalizar la conexión
 }
 
 
